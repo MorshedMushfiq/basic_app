6c14165e9a8854102c4cf9f3510a29a9afe97d1b
MorshedMushfiq <morshedm.dev@gmail.com>
2024-11-05 21:34:48 +0600

    fully basic app for project but need to changes in models.py and admin.py

---
+++ myProject/myApp/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path
+
+from myApp.views import *
+
+urlpatterns = [
+    path('',signupPage,name="signupPage"),
+    path("signInPage/", signInPage, name="signInPage"),
+    path("homePage/", homePage, name="homePage"),
+    path("logoutPage/", logoutPage, name="logoutPage"),
+    path("ProfilePage/", profilePage, name="profilePage"),
+    path("editProfile/", editProfile, name="editProfile"),
+
+    
+    
+]
